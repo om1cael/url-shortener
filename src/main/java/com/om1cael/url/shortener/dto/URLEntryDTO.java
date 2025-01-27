2514a3e2f5dcb64d72d06ffda19981895338f669
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-27 18:48:49 -0300

    feat: include short url on the url entry dto

--- src/main/java/com/om1cael/url/shortener/dto/URLEntryDTO.java
+++ src/main/java/com/om1cael/url/shortener/dto/URLEntryDTO.java
@@ -1,5 +1,11 @@
 package com.om1cael.url.shortener.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
-public record URLEntryDTO(@NotNull String URL) {}
+public record URLEntryDTO
+    (
+        @NotNull String URL,
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY) String shortURL
+    )
+{}
